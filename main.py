a74b6df8a3feaa39dd3ca1f7219697d19a10dddf
110119076 <venkataped@gmail.com>
2025-01-04 00:46:17 +0530

    feat:main.py updated

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.iplPredictor import logger
+from iplPredictor import logger
+from iplPredictor.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("You can track this project from the logs")
+# logger.info("You can track this project from the logs")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
